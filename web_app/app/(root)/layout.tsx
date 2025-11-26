d90596220661022bac6808c48879485f2cdd98b3
Vincent Sham <vincentsham@hotmail.com>
2025-11-26 05:29:07 -0500

    complete header, footer, home page, logo and icon

--- web_app/app/(root)/layout.tsx
+++ web_app/app/(root)/layout.tsx
@@ -1,13 +1,15 @@
 import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
 const Layout = async ({ children }: { children : React.ReactNode }) => {
 
     return (
-        <main className="min-h-screen text-gray-400">
+        <main className="min-h-screen flex flex-col text-gray-400">
             <Header />
-            <div className="container py-10">
+            <div className="container py-10 flex-grow">
                 {children}
             </div>
+            <Footer />
         </main>
     )
 }
